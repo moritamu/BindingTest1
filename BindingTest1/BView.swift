af3534d062cef6d58c3a56f646cef2283f303cd9
morisum <moris@r.sannet.ne.jp>
2023-09-19 21:27:26 +0900

    コメントを入れた

--- BindingTest1/BView.swift
+++ BindingTest1/BView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct BView: View {
     @Binding var secondsRemaining: Int
-    @Binding var secondsFirst: Int
+    
     @Binding var isTimerRunning: Bool
+    @State var secondsFirst = 10
+    //    コンピューテッドプロパティでsecondsFirstを作る
+//    var secondsFirst: Int {
+//        secondsRemaining
+//    }
     
     private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
@@ -19,26 +24,32 @@ struct BView: View {
             ProgressView(value: Double(secondsRemaining) / Double(secondsFirst)) { Text("あと\(secondsRemaining)秒") }
             Text("\(secondsRemaining)")
                 .font(.largeTitle)
+//            下の条件式がうまく動かない。カウンターが最初に戻らない。デバッガーを使う必要がある。230921
                 .onReceive(timer){_ in
-                    if isTimerRunning && secondsRemaining > 0{
+                    if isTimerRunning && secondsRemaining > 0 {
                         secondsRemaining -= 1
-                    } else if secondsRemaining > 0{
-                        isTimerRunning = false
                     } else if isTimerRunning {
                         isTimerRunning = false
+                        secondsRemaining = secondsFirst
+                    } else if secondsRemaining > 0 {
+                        isTimerRunning = false
                     } else {
+//                        ここは来ない
                         isTimerRunning = false
                         secondsRemaining = secondsFirst
-                        //                        secondsRemaining = secondsFirst
                     }
                 }
                 .onTapGesture {
                     self.isTimerRunning.toggle()
                 }
         }
+//        表示された時にsecondsFirstとして最初の値を取っておく
+        .onAppear {
+            secondsFirst = secondsRemaining
+        }
         
     }
-
+    
 }
 
 //struct BView_Previews: PreviewProvider {
--- BindingTest1/ContentView.swift
+++ BindingTest1/ContentView.swift
@@ -8,23 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var secondsF = 10
+//    @State private var secondsF = 10
     @State private var secondR = 10
     @State var isTimer = false
     @State var isTimer2 = false
-    @State private var secondsF2 = 10
+//    @State private var secondsF2 = 10
     @State private var secondR2 = 10
-
+//2つのタイマーには２種類の変数をBindingで渡すと2つのタイマーとして動く
+//    isTimerだけでは2つにならない
     var body: some View {
         VStack {
-            BView(secondsRemaining: $secondR, secondsFirst: $secondsF, isTimerRunning: $isTimer)
+            BView(secondsRemaining: $secondR, isTimerRunning: $isTimer)
             Button(action: {
                 isTimer.toggle()
             }){
                 isTimer ? Text("タイマーStop") :  Text("タイマー開始")
             }
             .font(.title)
-            BView(secondsRemaining: $secondR2, secondsFirst: $secondsF2, isTimerRunning: $isTimer2)
+            BView(secondsRemaining: $secondR2, isTimerRunning: $isTimer2)
             Button(action: {
                 isTimer2.toggle()
             }){
