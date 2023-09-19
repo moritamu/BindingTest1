d56b8c762389b9ad62342a2cf0b804e93e82f61a
morisum <moris@r.sannet.ne.jp>
2023-09-19 10:24:35 +0900

    Initial Commit

---
+++ BindingTest1/BindingTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  BindingTest1App.swift
+//  BindingTest1
+//
+//  Created by MsMacM on 2023/09/19.
+//
+
+import SwiftUI
+
+@main
+struct BindingTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
