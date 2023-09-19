9df90a66e682553e20d02571fdb443d276fbc3d1
morisum <moris@r.sannet.ne.jp>
2023-09-19 16:16:10 +0900

    別ファイルのタイマーを動かすために＠Bindingを使う。

--- BindingTest1/ContentView.swift
+++ BindingTest1/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var secondsF = 10
+    @State private var secondR = 10
+    @State var isTimer = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            BView(secondsRemaining: $secondR, secondsFirst: $secondsF, isTimerRunning: $isTimer)
+            Button(action: {
+                isTimer.toggle()
+            }){
+                isTimer ? Text("タイマーStop") :  Text("タイマー開始")
+            }
+            .font(.title)
         }
         .padding()
     }
